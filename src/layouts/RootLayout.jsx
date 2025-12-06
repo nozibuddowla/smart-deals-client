f53bf1eae429f4c782d2c4a9e66a7ccf50298186
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-12-07 00:22:25 +0600

    create routes and basic pages

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import { Outlet } from 'react-router';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <header>
+                <Navbar />
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+};
+
+export default RootLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import RootLayout from './layouts/RootLayout.jsx'
+import Home from './components/Home/Home.jsx'
+import AllProducts from './components/AllProducts/AllProducts.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, 
+      {
+        path: "/all-products",
+        element: <AllProducts />
+      }
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
