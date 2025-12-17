893f844447e0319edee737f44978463dba86ea6e
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-12-18 05:06:16 +0600

    create custom jwt token

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -46,6 +46,25 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+
+      if (currentUser) {
+        const loggedUser = { email: currentUser.email };
+
+        fetch(`${import.meta.env.VITE_API_URL}/getToken`, {
+          method: "POST",
+
+          headers: {
+            "content-type": "application/json",
+          },
+
+          body: JSON.stringify(loggedUser),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // console.log("after getting token", data);
+          });
+      }
+
       setLoading(false);
     });
 
