d75ac4a9b1bb4cb13304c894f04ba4c93e3b2173
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-12-22 23:26:39 +0600

    add response interceptor handle 401 and 403 to logout user for bad request

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import useAuth from "./useAuth";
 import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const instance = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}`,
 });
 
 const useAxiosSecure = () => {
-  const { user } = useAuth();
+  const { user, signOutUser } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
     //  set token in the header for all the api call using axiosSecure hook
@@ -15,15 +17,38 @@ const useAxiosSecure = () => {
     const requestInterceptor = instance.interceptors.request.use((config) => {
       // console.log(config);
 
-      config.headers.authorization = `Bearer: ${user.accessToken}`;
+      const token = user.accessToken;
+
+      if (token) {
+        config.headers.authorization = `Bearer: ${token}`;
+      }
 
       return config;
     });
 
+    // response interceptor
+    const responseInterceptor = instance.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (err) => {
+        console.log(err);
+        const status = err.status;
+        if (status === 401 || status === 403) {
+          console.log("log out the user for bad request");
+          signOutUser().then(() => {
+            // navigate user to the login page
+            navigate("/login");
+          });
+        }
+      }
+    );
+
     return () => {
       instance.interceptors.request.eject(requestInterceptor);
+      instance.interceptors.response.eject(responseInterceptor);
     };
-  }, [user]);
+  }, [user, signOutUser, navigate]);
 
   return instance;
 };
